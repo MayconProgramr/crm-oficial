83c2580b40c883577a54ca476550bf449238c336
MayconProgramr <may.con.00@hotmail.com>
2023-07-04 18:22:25 -0300

    Teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import { ptBR } from "@material-ui/core/locale";
 
 const App = () => {
-	const [locale, setLocale] = useState();
+  const [locale, setLocale] = useState();
 
   const theme = createTheme(
     {
@@ -17,7 +17,7 @@ const App = () => {
         },
         '&::-webkit-scrollbar-thumb': {
           //boxShadow: 'inset 0 0 6px rgba(170, 0, 126, 126)',
-          backgroundColor: '#2DDD7F',
+          backgroundColor: '#dd2d2d',
         },
       },
       palette: {
@@ -40,26 +40,27 @@ const App = () => {
       textColorMenu: {
         primary: { main: "#000000" },
         secondary: { main: "#2DDD7F" }
-      
-      },    },
+
+      },
+    },
     locale
   );
 
-	useEffect(() => {
-		const i18nlocale = localStorage.getItem("i18nextLng");
-		const browserLocale =
-			i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
+  useEffect(() => {
+    const i18nlocale = localStorage.getItem("i18nextLng");
+    const browserLocale =
+      i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
 
-		if (browserLocale === "ptBR") {
-			setLocale(ptBR);
-		}
-	}, []);
+    if (browserLocale === "ptBR") {
+      setLocale(ptBR);
+    }
+  }, []);
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Routes />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  );
 };
 
 export default App;
