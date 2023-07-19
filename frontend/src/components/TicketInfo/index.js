64b7ffcdfc1383fc3970c2e83e363b2e76d5b363
MayconProgramr <may.con.00@hotmail.com>
2023-07-18 21:06:23 -0300

    Ajustado para exibir o numero do protocolo no cabeçalho da conversa.

--- frontend/src/components/TicketInfo/index.js
+++ frontend/src/components/TicketInfo/index.js
@@ -12,7 +12,7 @@ const TicketInfo = ({ contact, ticket, onClick }) => {
 	useEffect(() => {
 		if (contact) {
 			setContactName(contact.name);
-			if(document.body.offsetWidth < 600) {
+			if (document.body.offsetWidth < 600) {
 				if (contact.name.length > 10) {
 					const truncadName = contact.name.substring(0, 10) + '...';
 					setContactName(truncadName);
@@ -23,7 +23,7 @@ const TicketInfo = ({ contact, ticket, onClick }) => {
 		if (user && contact) {
 			setUserName(`${i18n.t("messagesList.header.assignedTo")} ${user.name}`);
 
-			if(document.body.offsetWidth < 600) {
+			if (document.body.offsetWidth < 600) {
 				setUserName(`${user.name}`);
 			}
 		}
@@ -37,7 +37,7 @@ const TicketInfo = ({ contact, ticket, onClick }) => {
 			titleTypographyProps={{ noWrap: true }}
 			subheaderTypographyProps={{ noWrap: true }}
 			avatar={<Avatar src={contact.profilePicUrl} alt="contact_image" />}
-			title={`${contactName} #${ticket.id}`}
+			title={`${contactName} #${ticket.protocolo}`}
 			subheader={ticket.user && `${userName}`}
 		/>
 	);
